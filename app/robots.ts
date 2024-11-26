7df5e164498b528f89ebce4bfdc287ad12a48c67
mehmetaltann@gmail.com <mehmetaltann@gmail.com>
2024-11-26 08:30:08 +0300

    26.11.2024

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: "/admin/",
     },
     sitemap: `${siteUrl}/sitemap.xml`,
   };
