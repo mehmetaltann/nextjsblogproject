dd2d15e0288219b71d4ea3aa5320a44f9902bf28
mehmetaltann <mehmetaltann@gmail.com>
2025-10-31 16:16:06 +0300

    31.10.2025

--- Components/Admin/Write/TextEditor.tsx
+++ Components/Admin/Write/TextEditor.tsx
@@ -1,60 +1,36 @@
-import { useRef, useMemo, useContext, SetStateAction, useEffect } from "react";
+import { useRef, useMemo, useContext, SetStateAction } from "react";
 import { AdminContext } from "@/store/AdminContext";
 import dynamic from "next/dynamic";
-import { registerFlickityPlugin } from "@/Components/ui/joditFlickityPlugin";
-import Head from "next/head";
 
-const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
+const JoditEditor = dynamic(() => import("jodit-react"), {
+  ssr: false,
+});
 
 const TextEditor = () => {
   const context = useContext(AdminContext);
-  if (!context)
+  if (!context) {
     throw new Error(
       "useClientContext must be used within a ClientContextProvider"
     );
-
+  }
   const { setDescription, description } = context;
   const editor = useRef(null);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      registerFlickityPlugin();
-    }
-  }, []);
-
-  const config = useMemo(() => ({
-    readonly: false,
-    placeholder: "Yazınız...",
-    height: 600,
-    buttons: [
-      "bold",
-      "italic",
-      "|",
-      "flickitySlider",
-      "|",
-      "ul",
-      "ol",
-      "link",
-    ],
-    uploader: {
-      insertImageAsBase64URI: true,
-      imagesExtensions: ["jpg", "png", "jpeg", "gif", "svg", "webp"],
-    },
-  }), []);
+  const config = useMemo(
+    () => ({
+      readonly: false,
+      placeholder: "Yazınız...",
+      height: 600,
+      uploader: {
+        insertImageAsBase64URI: true,
+        imagesExtensions: ["jpg", "png", "jpeg", "gif", "svg", "webp"],
+      },
+    }),
+    []
+  );
 
   return (
     <div tabIndex={1}>
-      {/* Flickity CSS + JS sadece client-side */}
-      {typeof window !== "undefined" && (
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/flickity@2/dist/flickity.min.css"
-          />
-          <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-        </Head>
-      )}
-
       <JoditEditor
         ref={editor}
         value={description}
@@ -62,7 +38,7 @@ const TextEditor = () => {
         onBlur={(newContent: SetStateAction<string>) =>
           setDescription(newContent)
         }
-        onChange={() => {}}
+        onChange={(newContent) => {}}
       />
     </div>
   );
