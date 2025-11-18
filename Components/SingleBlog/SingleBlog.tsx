ff7977abf4ada0dac4aa4b71f9cf581d236ec16d
mehmetaltann <mehmetaltann@gmail.com>
2025-11-18 10:57:34 +0300

    18.11.2025

--- Components/SingleBlog/SingleBlog.tsx
+++ Components/SingleBlog/SingleBlog.tsx
@@ -1,8 +1,5 @@
 "use client";
-<<<<<<< HEAD
-=======
 
->>>>>>> 0728e0cdedab5a77956c62570a778f5ae0bfc647
 import Comments from "./Comments/Comments";
 import SocialMediaShareSet from "../Layouts/SocialMediaShareSet";
 import parse from "html-react-parser";
@@ -12,10 +9,6 @@ import { CldImage } from "next-cloudinary";
 import { getFormatDate } from "@/lib/utils/helpers";
 import { CommentType, PostType } from "@/lib/types/types";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0728e0cdedab5a77956c62570a778f5ae0bfc647
 interface SingleBlogProps {
   blog: PostType;
   sameCategoryBlogs: PostType[];
@@ -29,9 +22,9 @@ const SingleBlog = ({
   comments,
   siteUrl,
 }: SingleBlogProps) => {
-  const filteredBlogsByCategory = sameCategoryBlogs.filter((item) => {
-    return item.title !== blog.title;
-  });
+  const filteredBlogsByCategory = sameCategoryBlogs.filter(
+    (item) => item.title !== blog.title
+  );
 
   return (
     <AnimationWrapper
@@ -70,10 +63,6 @@ const SingleBlog = ({
       </div>
       <div className="space-y-4 text-zinc-700 mb-4 w-full">
         {parse(blog.description)}
-<<<<<<< HEAD
-=======
-          
->>>>>>> 0728e0cdedab5a77956c62570a778f5ae0bfc647
       </div>
       <div className="mt-2 self-end">
         <SocialMediaShareSet
