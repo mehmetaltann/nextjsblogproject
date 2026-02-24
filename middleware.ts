3d262fd8e3b33f2dbe11cd18c81d0fabe10a27cb
mehmetaltann <mehmetaltann@gmail.com>
2026-02-24 09:54:40 +0300

    24.02.2026

--- middleware.ts
+++ middleware.ts
@@ -16,5 +16,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/admin", "/admin/register", "/admin/categories", "/admin/write"],
 };
